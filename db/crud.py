4bf62f0683b3e53f27e004e1549c980bc777773d
Anshul Nigam <anshulnigam111@gmail.com>
2025-07-06 13:36:56 +0530

    Add files via upload

---
+++ db/crud.py
@@ -0,0 +1,26 @@
+
+from db.models import Task
+from db.sessions import SessionLocal
+
+def create_task(task_id: str, youtube_url: str):
+    db = SessionLocal()
+    task = Task(id=task_id, youtube_url=youtube_url)
+    db.add(task)
+    db.commit()
+    db.refresh(task)
+    db.close()
+    return task
+
+def update_task_status(task_id: str, status: str):
+    db = SessionLocal()
+    task = db.query(Task).filter(Task.id == task_id).first()
+    if task:
+        task.status = status
+        db.commit()
+    db.close()
+
+def get_task(task_id: str):
+    db = SessionLocal()
+    task = db.query(Task).filter(Task.id == task_id).first()
+    db.close()
+    return task
